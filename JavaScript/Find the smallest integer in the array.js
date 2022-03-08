6761604cced6757cfcbc58080690ec2355e429d4
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2022-03-07 22:47:22 -0500

    find the smallest array solution 2

--- JavaScript/Find the smallest integer in the array.js
+++ JavaScript/Find the smallest integer in the array.js
@@ -1,6 +1,13 @@
 class SmallestIntegerFinder {
   findSmallestInt(args) {
-    args.sort((a,b) => a -b)
+    args.sort((a,b) => a - b)
     return args[0]
+  }
+}
+
+// solution 2
+class SmallestIntegerFinder {
+  findSmallestInt(args){
+    return Math.min(...args)
   }
 }
