2e5072dad555b4ac0036985159c802ee2fdd810c
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2022-03-17 20:48:11 -0400

    simple multiplication

---
+++ JavaScript/Simple multiplication.js
@@ -0,0 +1,15 @@
+function simpleMultiplication(number){
+    
+    if(number % 2 === 0){
+        return number *8
+    }
+    else{
+        return number* 9;
+    }    
+}
+
+
+// ternary operator
+// function simpleMultiplication(number) {
+//     return number % 2 === 0 ? number * 8 : number * 9
+// }
